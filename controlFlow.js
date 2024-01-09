cc35e2f4479e5e115a77277e867e4eb664248c94
Ritik Kumar <ritik.kumar@simbaquartz.com>
2024-01-09 10:01:13 +0530

    aa

---
+++ controlFlow.js
@@ -0,0 +1,86 @@
+// // >,<,==,===,!=,<=,>=,!,||,&&
+
+// // const amount = 10;
+// // if (amount >= 10) {
+// //   console.log("hjhduh");
+// // }
+
+// // const amount = 20;
+// // const paisa = 50;
+
+// // if (amount === 200 || paisa === 50) {
+// //   console.log("Paisa hi paisa");
+// // } else {
+// //   console.log("humne greebi dekhi hai");
+// // }
+
+// // const paisa = 1000;
+
+// // if (paisa < 500) {
+// //   console.log("Paisa is less than 500");
+// // } else if (paisa < 700) {
+// //   console.log("Paisa is less than 700");
+// // } else if (paisa < 1200) {
+// //   console.log("Paisa is less than 1200");
+// // } else {
+// //   console.log("Paisa is very big");
+// // }
+
+// // if (userLogin.length > 0) {
+// //   console.log("Yes mai login aaa");
+// // } else {
+// //   console.log("No mai login nai aa");
+// // }
+
+// // const yoyo = true;
+
+// // if (true) {
+// //   console.log("YOYO ghr aja");
+// // }
+
+// // split
+
+// // const paragraph = ["Hello&Amrit&Ki&haall&aa"];
+// // const newParagraph = paragraph[0].split("&");
+// // console.log(newParagraph[1]);
+
+// // Ternary Operator
+
+// condition ? true : false;
+
+// const num = 120;
+
+// num >= 120
+//   ? console.log("120 toh mhaan hai")
+//   : console.log("120 nu kon pushda hai");
+
+// // switchCase Statement
+// const month = 5;
+// switch (month) {
+//   case 1:
+//     console.log("January");
+//     break;
+//   case 2:
+//     console.log("Feb");
+//     break;
+//   case 3:
+//     console.log("March");
+//     break;
+//   case 4:
+//     console.log("April");
+//     break;
+
+//   default:
+//     console.log("Mujhe koi rokke dekhaiii");
+//     break;
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//   //   const element = i;
+//   //   console.log(`Value of i is = ${element}`);
+//   for (let j = 1; j <= 10; j++) {
+//     // const element2 = j;
+//     // console.log(`Value of j is = ${element2} and value of i is = ${element}`);
+//     console.log(i + "*" + j + " = " + i * j);
+//   }
+// }
---
+++ looping.js
@@ -0,0 +1,48 @@
+// for (let i = 0; i <= 10; i++) {
+//   const val = i;
+//   if (val == 5) {
+//     console.log("5 aa gya");
+//   }
+//   console.log(val);
+// }
+
+// for (let i = 0; i < 2; i++) {
+//   const element = i;
+//   console.log(`This is a outer loop ${element}`);
+//   for (let j = 0; j < 2; j++) {
+//     const val = j;
+//     console.log(`This is a inner loop ${val} and also a outer loop ${element}`);
+//   }
+// }
+
+// for (let i = 5; i <= 5; i++) {
+//   for (let j = 1; j <= 10; j++) {
+//     console.log(i + "*" + j + " = " + i * j);
+//   }
+// }
+
+// Reverse Method
+// let firstName = "Amrit";
+// let reversed_firstname = firstName.split("").reverse().join("");
+// console.log(reversed_firstname);
+
+// Break Statement
+
+// for (let i = 0; i < 10; i++) {
+//   if (i == 6) {
+//     break;
+//   }
+//   const element = i;
+//   console.log(element);
+// }
+
+//Continue Statement
+
+// for (let i = 0; i < 10; i++) {
+//   if (i == 7) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// condition ? true : false;/
